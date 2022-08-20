49f2a76961bb400409dae26d3bcac1134c772d5f
Anup Jadhav <anup.jadhav@gmail.com>
2022-08-20 06:38:59 +0200

    type conversions

---
+++ conversion/src/main.rs
@@ -0,0 +1,54 @@
+#![allow(dead_code)]
+use std::convert::From;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+
+#[derive(Debug, PartialEq)]
+struct EvenNumber(i32);
+
+impl TryFrom<i32> for EvenNumber {
+    type Error = ();
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        if value % 2 == 0 {
+            Ok(EvenNumber(value))
+        } else {
+            Err(())
+        }
+    }
+}
+
+fn main() {
+    // using from <- notice lowercase f in from
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+
+    // Into trait is the reciprocal of the From trait
+    // Into will call From trait if you have implemented it
+    let i = 5; 
+    let num: Number = i.into();
+    println!("My number after into is {:?}", num);
+
+    // TryFrom
+    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(5), Err(()));
+
+    // TryInto
+    let result: Result<EvenNumber, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
+    let result: Result<EvenNumber, ()> = 5i32.try_into();
+    assert_eq!(result, Err(()));
+}
