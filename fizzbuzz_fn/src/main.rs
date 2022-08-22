63d34651759fc997bcc4fd63d215c2ba76baab71
Anup Jadhav <anup.jadhav@gmail.com>
2022-08-22 07:10:39 +0200

    updated fizzbuzz function

---
+++ fizzbuzz_fn/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    fizzbuzz_to(100);
+}
+
+fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
+    // Corner case, early return
+    if rhs == 0 {
+        return false;
+    }
+
+    // This is an expression, therefore
+    // return keyword is not required
+    lhs % rhs == 0
+}
+
+fn fizzbuzz(n: u32) {
+    if is_divisible_by(n, 15) {
+        println!("fizzbuzz");
+    } else if is_divisible_by(n, 3) {
+        println!("fizz");
+    } else if is_divisible_by(n, 5) {
+        println!("buzz");
+    } else {
+        println!("{}", n);
+    }
+}
+
+fn fizzbuzz_to(n: u32) {
+    for i in 1..=n {
+        fizzbuzz(i);
+    }
+}
