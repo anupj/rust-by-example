12038caed012593408c714ff09df085431db78f7
Anup Jadhav <anup.jadhav@gmail.com>
2022-09-03 11:16:24 +0100

    option & unwrap with combinators

---
+++ combinators-error-handling/src/main.rs
@@ -0,0 +1,71 @@
+#![allow(dead_code)]
+
+#[derive(Debug)]
+enum Food {
+    Apple,
+    Carrot,
+    Potato,
+}
+
+#[derive(Debug)]
+struct Peeled(Food);
+
+#[derive(Debug)]
+struct Chopped(Food);
+
+#[derive(Debug)]
+struct Cooked(Food);
+
+// Peeling food. If there isn't any, then return `None`.
+// Otherwise, return the peeled food.
+fn peel(food: Option<Food>) -> Option<Peeled> {
+    match food {
+        Some(food) => Some(Peeled(food)),
+        None             => None,
+    }
+}
+
+// Chopped food. If there isn't any, then return `None`.
+// Otherwise, return the chopped food.
+fn chop(food: Option<Peeled>) -> Option<Chopped> {
+    match food {
+        Some(Peeled(food)) => Some(Chopped(food)),
+        None                     => None,
+    }
+}
+
+// Cooking food
+fn cook(chopped: Option<Chopped>) -> Option<Cooked> {
+    chopped.map(|Chopped(food)| Cooked(food))
+}
+
+// A function to peel, chop, and cook food all in sequence
+fn process(food: Option<Food>) -> Option<Cooked> {
+    food.map(|f| Peeled(f))
+        .map(|Peeled(f)| Chopped(f))
+        .map(|Chopped(f)| Cooked(f))
+}
+
+// Check whether there's food or not
+// before trying to eat it!
+fn eat(food: Option<Cooked>) {
+    match food {
+        Some(food) => println!("Mmm. I love {:?}", food),
+        None               => println!("Oh no! It wasn't edible."),
+    }
+}
+
+fn main() {
+    let apple = Some(Food::Apple);
+    let carrot = Some(Food::Carrot);
+    let potato = None;
+
+    let cooked_apple = cook(chop(peel(apple)));
+    let cooked_carrot = cook(chop(peel(carrot)));
+    // much simpler looking `process()` now.
+    let cooked_potato = process(potato);
+
+    eat(cooked_apple);
+    eat(cooked_carrot);
+    eat(cooked_potato);
+}
